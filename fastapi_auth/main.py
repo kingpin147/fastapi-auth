636ad1352a2c5e7676757318114267a8add993c9
kingpin147 <nomiking0072012@gmail.com>
2024-06-13 15:53:48 +0500

    update

--- fastapi_auth/main.py
+++ fastapi_auth/main.py
@@ -4,7 +4,22 @@ from datetime import datetime, timedelta
 from fastapi.security import OAuth2, OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from typing import Annotated
 
+
+
 app = FastAPI()
+
+
+# Multiple microservice - 1 Auth Microservice
+#Step1: Wrapper endpoint that takes form data and call auth service login endpoint
+#Step2: Import OAuth2PasswordBearer - tokenUrl = Wrapper endpoint api path
+#Step3: oauth2_scheme = Pass to all endpoints in dependencies
+
+
+
+
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 fake_users_db = {"admin": {"username": "admin", "hashed_password": "admin", "email": "admin@admin.com", "full_name": "admin"},
     "user": {"username": "nouman", "hashed_password": "attique", "email": "nouman@nouman.com", "full_name": "nouman"}
@@ -62,9 +77,13 @@ def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends(OAuth2Password
     return {"username": form_data.username, "generated_access_token": generated_access_token}  
 
 @app.get("/all-users")
-def get_all_users():
+def get_all_users(token: Annotated[str, Depends(oauth2_scheme)]):
     return fake_users_db
 
+@app.get("/special-items")
+def get_special_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    decode_data = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    return {"special": "items", "decode_data": decode_data, "token": token}
 
 
 
@@ -92,7 +111,7 @@ def create_access_token(subject: str , expires_delta: timedelta) -> str:
     return encoded_jwt
 
 
-@app.get("/new_route")
+@app.get("/access_token")
 def get_access_token(user_name: str):
     """
     Endpoint to get an access token for a given user.
@@ -113,3 +132,17 @@ def get_access_token(user_name: str):
     
     # Step 3: Return the access token in a dictionary
     return {"access_token": access_token}
+
+
+
+def decode_access_token(access_token: str):
+    decoded_jwt = jwt.decode(access_token, SECRET_KEY, algorithms=[ALGORITHM])
+    return decoded_jwt
+
+@app.get("/decode_token")
+def decoding_token(access_token: str):
+    try:
+        decoded_token_data = decode_access_token(access_token)
+        return {"decoded_token": decoded_token_data}
+    except JWTError as e:
+        return {"error": str(e)}
